4f42054f25e2b81b0bf6a72c1e4faa0cf3f4b227
s4eedb <m.babaee@apk-group.net>
2022-06-11 14:29:14 +0430

    feat: add readme

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # dasl
 work with debian apt sources list
+
+# example
+
+```go
+package main
+
+import (
+	"fmt"
+
+	"github.com/S4eedb/dasl"
+)
+
+func main() {
+
+	source, err := dasl.ParseSourceLine("deb http://deb.debian.org/debian buster-updates main contrib non-free")
+	if err != nil {
+		fmt.Print(err)
+	} else {
+		fmt.Print(source)
+	}
+}
+
+```
--- examples/source_string_parser/main.go
+++ examples/source_string_parser/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/S4eedb/dasl"
 )
 
 func main() {
 
-	dasl.ParseSourceLine("deb http://deb.debian.org/debian buster-updates main contrib non-free")
+	source, err := dasl.ParseSourceLine("deb http://deb.debian.org/debian buster-updates main contrib non-free")
+	if err != nil {
+		fmt.Print(err)
+	} else {
+		fmt.Print(source)
+	}
 }
